389661f03a0328e5131b5ffe7996f4d4c43289f5
lbrown169 <158978873+lbrown169@users.noreply.github.com>
2025-03-03 16:05:39 -0500

    fix: Remove redundant calls to driver destroy

--- backend/tests/repo/MongoUserRepository.test.js
+++ backend/tests/repo/MongoUserRepository.test.js
@@ -11,50 +11,43 @@ beforeEach(async () => {
     driver = loadDatabaseDriver();
 })
 
-afterAll(async () => {
+afterEach(async () => {
     await driver.destroy();
 })
 
 test("Tests getting a mongo user by ID", async () => {
     let result = await driver.userRepository.GetById("67bf9446fc89bb9e490cfe40");
     expect(result).toBeDefined();
-    await driver.destroy();
 })
 
 test("Tests failing to get a mongo user by ID", async () => {
     let result = await driver.userRepository.GetById("67bf9446fc89bb9eaaaaaaaa");
     expect(result).not.toBeDefined();
-    await driver.destroy();
 })
 
 test("Tests getting a mongo user only by Email", async () => {
     let result = await driver.userRepository.GetByEmail("john.smith@example.com");
     expect(result).toBeDefined();
-    await driver.destroy();
 })
 
 test("Tests failing to get a mongo user only by Email", async () => {
     let result = await driver.userRepository.GetByEmail("a wrong email");
     expect(result).not.toBeDefined();
-    await driver.destroy();
 })
 
 test("Tests getting a mongo user by Email and Password", async () => {
     let result = await driver.userRepository.GetByEmailAndPassword("john.smith@example.com", "SecurePassword123!");
     expect(result).toBeDefined();
-    await driver.destroy();
 })
 
 test("Tests failing to get a mongo user by Email and Password", async () => {
     let result = await driver.userRepository.GetByEmailAndPassword("john.doe@example.com", "VeryWrongPassword");
     expect(result).not.toBeDefined();
-    await driver.destroy();
 })
 
 test("Tests registering a mongo user", async () => {
     let result = await driver.userRepository.Register(new UserRegistration("Jane Smith", "jane.smith@example.com", "AnotherPassword!"));
     expect(result).toBeDefined();
-    await driver.destroy();
 })
 
 test("Tests failing to register a mongo user", async () => {
