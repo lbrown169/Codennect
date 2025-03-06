95b98f6a2e673cb5a0d7d1b6a01fba8f4bb44dc9
freshPressedOlive <olive.arderius@gmail.com>
2025-03-06 17:25:11 -0500

    moved files

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,67 @@
 import { config } from "dotenv";
 import { loadDatabaseDriver } from "./repo/Driver";
+import { User, UserRegistration, UserRepository } from "./domain/User";
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
 config();
 
 let driver = loadDatabaseDriver();
+
+app.post('/api/login', async (req, res, next) =>
+{
+	// incoming: email, password
+	// outgoing: id, name, error
+
+	var error = '';
+	const { email, password } = req.body;
+	const db = driver;
+
+	const theUser = await db.userRepository.GetByEmailAndPassword(email, password);
+
+    // more specific error based on email OR password
+	if( theUser == null )
+	{
+		return res.status(400).json({ error: 'User not found!' });
+	}
+	
+	res.status(200).json({ id: theUser._id, name: theUser.name, error:''});
+});
+
+
+app.post('/api/register', async (req, res, next) =>
+{
+	// incoming: name, email, password
+	// outgoing: id, error
+		// return new credentials?
+	
+	var error = '';
+	const { name, email, password } = req.body;
+	const db = driver;
+
+	// make sure there's not already a user with this login
+	const existingUser = await db.userRepository.GetByEmail(email);
+	
+	// if there is one, send an error
+	if (existingUser != null)
+	{
+		return res.status(400).json( {error:'User already exists!'} );
+	}
+	
+	// create new user instance
+	const newUser = new UserRegistration(name, email, password);
+
+	// insert the new user into the database using UserRepo
+	const registeredUser = await db.userRepository.Register(newUser);
+
+	// return a successful registration message
+	res.status(201).json({error: 'User registered successfully!'});
+});
+
+app.listen(5001);
