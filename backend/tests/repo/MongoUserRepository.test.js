8d0ee191e61530c502c1901da7fae94b3117baa0
Logan Brown <158978873+lbrown169@users.noreply.github.com>
2025-03-06 06:23:14 -0500

    [Database] Add user repository (#1)
    * feat: Full user repository code and tests
    
    * fix: Fix driver not persisting between tests
    
    * fix: Remove redundant calls to driver destroy

---
+++ backend/tests/repo/MongoUserRepository.test.js
@@ -0,0 +1,56 @@
+import { config } from "dotenv";
+import { UserRegistration } from "../../src/domain/User";
+import { loadDatabaseDriver } from "../../src/repo/Driver";
+
+var driver;
+
+beforeEach(async () => {
+    process.env.EXPLICIT_USE_MONGO = "true";
+    config();
+
+    driver = loadDatabaseDriver();
+})
+
+afterEach(async () => {
+    await driver.destroy();
+})
+
+test("Tests getting a mongo user by ID", async () => {
+    let result = await driver.userRepository.GetById("67bf9446fc89bb9e490cfe40");
+    expect(result).toBeDefined();
+})
+
+test("Tests failing to get a mongo user by ID", async () => {
+    let result = await driver.userRepository.GetById("67bf9446fc89bb9eaaaaaaaa");
+    expect(result).not.toBeDefined();
+})
+
+test("Tests getting a mongo user only by Email", async () => {
+    let result = await driver.userRepository.GetByEmail("john.smith@example.com");
+    expect(result).toBeDefined();
+})
+
+test("Tests failing to get a mongo user only by Email", async () => {
+    let result = await driver.userRepository.GetByEmail("a wrong email");
+    expect(result).not.toBeDefined();
+})
+
+test("Tests getting a mongo user by Email and Password", async () => {
+    let result = await driver.userRepository.GetByEmailAndPassword("john.smith@example.com", "SecurePassword123!");
+    expect(result).toBeDefined();
+})
+
+test("Tests failing to get a mongo user by Email and Password", async () => {
+    let result = await driver.userRepository.GetByEmailAndPassword("john.doe@example.com", "VeryWrongPassword");
+    expect(result).not.toBeDefined();
+})
+
+test("Tests registering a mongo user", async () => {
+    let result = await driver.userRepository.Register(new UserRegistration("Jane Smith", "jane.smith@example.com", "AnotherPassword!"));
+    expect(result).toBeDefined();
+})
+
+test("Tests failing to register a mongo user", async () => {
+    let result = driver.userRepository.Register(new UserRegistration("Jane Smith", "john.smith@example.com", "AnotherPassword!"));
+    await expect(result).rejects.toThrow('already exists');
+})
---
+++ backend/tests/repo/StaticUserRepository.test.js
@@ -0,0 +1,59 @@
+import { UserRegistration } from "../../src/domain/User";
+import { loadDatabaseDriver } from "../../src/repo/Driver";
+
+test("Tests getting a static user by ID", async () => {
+    let driver = loadDatabaseDriver();
+
+    let result = await driver.userRepository.GetById("0");
+    expect(result).toBeDefined();
+})
+
+test("Tests failing to get a static user by ID", async () => {
+    let driver = loadDatabaseDriver();
+
+    let result = await driver.userRepository.GetById("100");
+    expect(result).not.toBeDefined();
+})
+
+test("Tests getting a static user by Email", async () => {
+    let driver = loadDatabaseDriver();
+
+    let result = await driver.userRepository.GetByEmail("jane.doe@example.com");
+    expect(result).toBeDefined();
+})
+
+test("Tests failing to get a static user by Email", async () => {
+    let driver = loadDatabaseDriver();
+
+    let result = await driver.userRepository.GetByEmail("a wrong email");
+    expect(result).not.toBeDefined();
+})
+
+test("Tests getting a static user by Email and Password", async () => {
+    let driver = loadDatabaseDriver();
+
+    let result = await driver.userRepository.GetByEmailAndPassword("john.doe@example.com", "SuperSecret123!");
+    expect(result).toBeDefined();
+})
+
+test("Tests failing to get a static user by Email and Password", async () => {
+    let driver = loadDatabaseDriver();
+
+    let result = await driver.userRepository.GetByEmailAndPassword("john.doe@example.com", "VeryWrongPassword");
+    expect(result).not.toBeDefined();
+})
+
+test("Tests registering a static user", async () => {
+    let driver = loadDatabaseDriver();
+
+    let result = await driver.userRepository.Register(new UserRegistration("Jane Smith", "jane.smith@example.com", "AnotherPassword!"));
+    expect(result).toBeDefined();
+})
+
+test("Tests failing to register a static user", async () => {
+    let driver = loadDatabaseDriver();
+
+    expect(async () => {
+        await driver.userRepository.Register(new UserRegistration("Jane Smith", "jane.doe@example.com", "AnotherPassword!"))
+    }).rejects.toThrow('already exists');
+})
