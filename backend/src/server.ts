a2c1c3faf1e8485612ac357f7266bf8981cecf06
lbrown169 <158978873+lbrown169@users.noreply.github.com>
2025-03-13 16:50:59 -0400

    feat: Add proper build commands

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,9 +2,10 @@ import { config } from "dotenv";
 import { loadDatabaseDriver } from "./repo/Driver";
 import { User, UserRegistration, UserRepository } from "./domain/User";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 app.use(cors());
@@ -14,54 +15,54 @@ config();
 
 let driver = loadDatabaseDriver();
 
-app.post('/api/login', async (req, res, next) =>
-{
-	// incoming: email, password
-	// outgoing: id, name, error
+app.use(express.static(path.join(__dirname, "../build")));
 
-	var error = '';
-	const { email, password } = req.body;
-	const db = driver;
+app.post("/api/login", async (req, res, next) => {
+    // incoming: email, password
+    // outgoing: id, name, error
 
-	const theUser = await db.userRepository.GetByEmailAndPassword(email, password);
+    var error = "";
+    const { email, password } = req.body;
+    const db = driver;
+
+    const theUser = await db.userRepository.GetByEmailAndPassword(
+        email,
+        password
+    );
 
     // more specific error based on email OR password
-	if( theUser == null )
-	{
-		return res.status(400).json({ error: 'User not found!' });
-	}
-	
-	res.status(200).json({ id: theUser._id, name: theUser.name, error:''});
+    if (theUser == null) {
+        return res.status(400).json({ error: "User not found!" });
+    }
+
+    res.status(200).json({ id: theUser._id, name: theUser.name, error: "" });
 });
 
+app.post("/api/register", async (req, res, next) => {
+    // incoming: name, email, password
+    // outgoing: id, error
+    // return new credentials?
 
-app.post('/api/register', async (req, res, next) =>
-{
-	// incoming: name, email, password
-	// outgoing: id, error
-		// return new credentials?
-	
-	var error = '';
-	const { name, email, password } = req.body;
-	const db = driver;
+    var error = "";
+    const { name, email, password } = req.body;
+    const db = driver;
 
-	// make sure there's not already a user with this login
-	const existingUser = await db.userRepository.GetByEmail(email);
-	
-	// if there is one, send an error
-	if (existingUser != null)
-	{
-		return res.status(400).json( {error:'User already exists!'} );
-	}
-	
-	// create new user instance
-	const newUser = new UserRegistration(name, email, password);
+    // make sure there's not already a user with this login
+    const existingUser = await db.userRepository.GetByEmail(email);
 
-	// insert the new user into the database using UserRepo
-	const registeredUser = await db.userRepository.Register(newUser);
+    // if there is one, send an error
+    if (existingUser != null) {
+        return res.status(400).json({ error: "User already exists!" });
+    }
 
-	// return a successful registration message
-	res.status(201).json({error: 'User registered successfully!'});
+    // create new user instance
+    const newUser = new UserRegistration(name, email, password);
+
+    // insert the new user into the database using UserRepo
+    const registeredUser = await db.userRepository.Register(newUser);
+
+    // return a successful registration message
+    res.status(201).json({ error: "User registered successfully!" });
 });
 
 app.listen(5001);
