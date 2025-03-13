e5c61b228748a7169702fcd545adb48e901ad1fe
SarahD3904 <sarahd3904@gmail.com>
2025-03-13 14:19:32 -0400

    Fixed fetch issue within signup

--- frontend/src/components/Signup.tsx
+++ frontend/src/components/Signup.tsx
@@ -64,7 +64,7 @@ function Signup()
 
         try
         {
-            const response = await fetch('http://localhost:5001/api/signup',
+            const response = await fetch('http://localhost:5001/api/register',
             { 
                 method: 'POST',
                 body: js,
