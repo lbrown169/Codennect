2262d774b8e0760d20d4de8bf76934bdfd333823
SarahD3904 <sarahd3904@gmail.com>
2025-03-13 17:01:49 -0400

    Fixed compile errors within App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Redirect, Navigate, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
