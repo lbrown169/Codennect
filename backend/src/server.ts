56e20b31287e90e457fc3a1dce300ed62203c5b4
lbrown169 <158978873+lbrown169@users.noreply.github.com>
2025-03-13 17:20:12 -0400

    fix: Fix subroutes not being routed correctly with express

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -15,54 +15,58 @@ config();
 
 let driver = loadDatabaseDriver();
 
-app.use(express.static(path.join(__dirname, "../build")));
-
 app.post("/api/login", async (req, res, next) => {
-    // incoming: email, password
-    // outgoing: id, name, error
+  // incoming: email, password
+  // outgoing: id, name, error
 
-    var error = "";
-    const { email, password } = req.body;
-    const db = driver;
+  var error = "";
+  const { email, password } = req.body;
+  const db = driver;
 
-    const theUser = await db.userRepository.GetByEmailAndPassword(
-        email,
-        password
-    );
+  const theUser = await db.userRepository.GetByEmailAndPassword(
+    email,
+    password
+  );
 
-    // more specific error based on email OR password
-    if (theUser == null) {
-        return res.status(400).json({ error: "User not found!" });
-    }
+  // more specific error based on email OR password
+  if (theUser == null) {
+    return res.status(400).json({ error: "User not found!" });
+  }
 
-    res.status(200).json({ id: theUser._id, name: theUser.name, error: "" });
+  res.status(200).json({ id: theUser._id, name: theUser.name, error: "" });
 });
 
 app.post("/api/register", async (req, res, next) => {
-    // incoming: name, email, password
-    // outgoing: id, error
-    // return new credentials?
+  // incoming: name, email, password
+  // outgoing: id, error
+  // return new credentials?
 
-    var error = "";
-    const { name, email, password } = req.body;
-    const db = driver;
+  var error = "";
+  const { name, email, password } = req.body;
+  const db = driver;
 
-    // make sure there's not already a user with this login
-    const existingUser = await db.userRepository.GetByEmail(email);
+  // make sure there's not already a user with this login
+  const existingUser = await db.userRepository.GetByEmail(email);
 
-    // if there is one, send an error
-    if (existingUser != null) {
-        return res.status(400).json({ error: "User already exists!" });
-    }
+  // if there is one, send an error
+  if (existingUser != null) {
+    return res.status(400).json({ error: "User already exists!" });
+  }
 
-    // create new user instance
-    const newUser = new UserRegistration(name, email, password);
+  // create new user instance
+  const newUser = new UserRegistration(name, email, password);
 
-    // insert the new user into the database using UserRepo
-    const registeredUser = await db.userRepository.Register(newUser);
+  // insert the new user into the database using UserRepo
+  const registeredUser = await db.userRepository.Register(newUser);
 
-    // return a successful registration message
-    res.status(201).json({ error: "User registered successfully!" });
+  // return a successful registration message
+  res.status(201).json({ error: "User registered successfully!" });
 });
+
+app.use(express.static(path.join(__dirname, "../build")));
+
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "..", "build", "index.html"))
+);
 
 app.listen(5001);
