912eb762905fb0244f52f1a4490f62be1e736f5d
SarahD3904 <sarahd3904@gmail.com>
2025-03-13 16:34:58 -0400

    Added redirect from dashboard if no user data found Also updated package.json

--- frontend/src/components/LoggedIn.tsx
+++ frontend/src/components/LoggedIn.tsx
@@ -1,6 +1,10 @@
 function LoggedIn()
 {
     var _ud = localStorage.getItem('user_data');
+    if(_ud == null) //redirect if user not found
+    {
+        window.location.href = '/';
+    }
     var userData = JSON.parse(_ud);
     var userId = userData.id;
     var userName = userData.name;
