8877c1337cbd2a77a3a780cc304fbf1e28660d5d
SarahD3904 <sarahd3904@gmail.com>
2025-03-26 10:30:25 -0400

    Added header to dashboard page

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,8 +1,10 @@
 import LoggedIn from "../components/LoggedIn";
+import HeaderBar from "../components/HeaderBar";
 const DashboardPage = () =>
 {
     return(
         <div>
+            <HeaderBar />
             <LoggedIn />
             <h1 className="text-center">Welcome!</h1>
         </div>
