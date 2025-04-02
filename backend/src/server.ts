0ab9a5ed24399a1faf6a0fd51416f997b26930d4
wfarnsworth110 <walkerfarnsworth@gmail.com>
2025-04-02 16:20:45 -0400

    Fixed error from unit testing by no longer allowing implicit typing for endpoint function parameters. Email verification near completion. Updated the email verification process to cooperate with the new database changes.
    TODO: Create transporter email

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import { config } from "dotenv";
+import { Request, Response, NextFunction } from 'express';
 import { loadDatabaseDriver } from "./repo/Driver";
 import { loadTransporter } from "./service/auth";
 import { User, UserRegistration, UserRepository } from "./domain/User";
@@ -23,7 +24,7 @@ let transporter = loadTransporter();
 const tokenStore = {};
 
 // Route to accept email and send verification
-app.post("/api/register", async (req, res) => {
+app.post("/api/register", async (req: Request, res: Response) => {
   
   const { email } = req.body;
   const db = driver;
@@ -31,11 +32,8 @@ app.post("/api/register", async (req, res) => {
   // Check for email, then see if one ties to a user
   if (!email) return res.status(400).json({ error: "Email is required" });
 
-  // make sure there's not already a user with this login
+  // make sure there's not already a registered user with this login
   const existingUser = await db.userRepository.GetByEmail(email);
-
-  // if there is one, send an error
-  // TODO Update to check for a "verified" flag within the user
   if (existingUser != null) {
     return res.status(400).json({ error: "User already exists!" });
   }
@@ -44,24 +42,12 @@ app.post("/api/register", async (req, res) => {
   What we will want is to store the email in a "purgatory" assignment until verification is done.
   Note: We will also want to see if the email is already in "purgatory", if it is, then we
   just send a new verification token to the same email*/
-  // create new user instance
-  const newUser = new UserRegistration("", email, "");
+  await db.verificationRepository.DeleteVerification(email); // Delete if applicable
+  const token = crypto.randomBytes(32).toString("hex"); // Generate token
+  const notYetUser = db.verificationRepository.RegisterVerification(email, token); // Add
 
-  // TODO Replace with inserting into "purgatory" (only if user isn't already in purgatory)
-  // insert the new user into the database using UserRepo
-  const registeredUser = await db.userRepository.Register(newUser);
-
-  // Now for the actual verification email
-  // Generate a simple random token
-  const token = crypto.randomBytes(32).toString("hex");
-
-  // TODO Store token in database for recalling it in verify-email endpoint
-  // Store the token and email temporarily
-  tokenStore[token] = { email, expires: Date.now() + 15 * 60 * 1000 }; // Expires in 15 mins
-
-  // Is this link good?
+  // Link to email
   const verificationLink = `http://cop4331.tech/verify-email?token=${token}`;
-
   // TODO This is all placeholder stuff to be resolved upon creation of proper transporter email
   const info = await transporter.sendMail({
     from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>',
@@ -71,33 +57,23 @@ app.post("/api/register", async (req, res) => {
   });
 
   console.log("Verification email sent: %s", info.messageId);
-
+"test"
   res.json({ message: "Verification email sent." });
 });
 
 // Route to verify token
-app.get("/api/verify-email", async (req, res) => {
+app.get("/api/verify-email", async (req: Request, res: Response) => {
   // This is why we need the token in the database
-  const { token, name, email, password } = req.query;
+  const { token, name, email, password } = req.body;
   const db = driver;
 
-  // TODO Subject to change
-  const record = tokenStore[token];
-  if (!record) {
+  // Validate token
+  const verifyAttempt = await db.verificationRepository.ValidateVerification(email, token);
+  if(!verifyAttempt) {
     return res.status(400).send("Invalid or expired token.");
   }
 
-  // Check for token expiration
-  if (record.expires < Date.now()) {
-    delete tokenStore[token];
-    return res.status(400).send("Token expired.");
-  }
-
-  // At this point, email is verified
-  const verifiedEmail = record.email;
-  delete tokenStore[token];
-
-  // create new user instance
+  // At this point, email is verified and we can register user
   const newUser = new UserRegistration(name, email, password);
 
   // insert the new user into the database using UserRepo
@@ -108,7 +84,7 @@ app.get("/api/verify-email", async (req, res) => {
 
 });
 
-app.post("/api/login", async (req, res, next) => {
+app.post("/api/login", async (req: Request, res: Response, next: NextFunction) => {
   // incoming: email, password
   // outgoing: id, name, error
 
@@ -129,7 +105,7 @@ app.post("/api/login", async (req, res, next) => {
   res.status(200).json({ id: theUser._id, name: theUser.name, error: "" });
 });
 
-app.post("/api/get-user-info", async (req, res) => {
+app.post("/api/get-user-info", async (req: Request, res: Response) => {
   // incoming: user id
   // outgoing: all the user info
 
@@ -179,7 +155,7 @@ app.post("/api/register", async (req, res, next) => {
 
 app.use(express.static(path.join(__dirname, "../build")));
 
-app.get("*", (req, res) =>
+app.get("*", (req: Request, res: Response) =>
   res.sendFile(path.resolve(__dirname, "..", "build", "index.html"))
 );
 
