78bd3a524187f9ef87980e24d71cc76a3fbe0ee2
lbrown169 <158978873+lbrown169@users.noreply.github.com>
2025-04-07 01:51:57 -0400

    fix: Missed rename in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ export NODE_ENV=production
 # Check if production environment file exists
 if [ ! -e "production.env" ]; then
     echo "No production environment found, creating..."
-    touch .env.production
+    touch production.env
     echo "NODE_ENV=production" >> production.env
     echo "Please paste your MONGODB URI:"
     read uri
