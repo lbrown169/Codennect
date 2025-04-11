5174a221f78147f5bc74dee214d8e7f6318432a9
Logan Brown <158978873+lbrown169@users.noreply.github.com>
2025-04-11 17:54:34 -0400

    Revert "Login register pages" (#13)

--- frontend/src/pages/SignupPage.tsx
+++ frontend/src/pages/SignupPage.tsx
@@ -1,11 +1,8 @@
 import Signup from '../components/Signup.tsx';
-import HeaderBar from '../components/HeaderBar.tsx';
-
 const SignupPage = () =>
 {
     return(
         <div>
-        <HeaderBar />
         <Signup />
         </div>
     );
