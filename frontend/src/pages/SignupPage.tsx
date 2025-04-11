317b4e79083237ddd90a39d5afa6e1b1e80e96f9
Logan Brown <158978873+lbrown169@users.noreply.github.com>
2025-04-11 18:03:17 -0400

    Revert "Revert "Login register pages"" (#14)
    * Revert "Revert "Login register pages" (#13)"
    
    This reverts commit 5328166517b1ffd1353610558685f028cd4e6f37.
    
    * Update HeaderBar.tsx
    
    ---------
    
    Co-authored-by: Artyboo-UCF <ad085158@ucf.edu>

--- frontend/src/pages/SignupPage.tsx
+++ frontend/src/pages/SignupPage.tsx
@@ -1,8 +1,11 @@
 import Signup from '../components/Signup.tsx';
+import HeaderBar from '../components/HeaderBar.tsx';
+
 const SignupPage = () =>
 {
     return(
         <div>
+        <HeaderBar />
         <Signup />
         </div>
     );
