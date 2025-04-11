cccd7453d0b6e070cb046563148b7528bb75ec06
Logan Brown <158978873+lbrown169@users.noreply.github.com>
2025-04-11 17:54:34 -0400

    Revert "Login register pages" (#13)

--- frontend/src/components/Signup.tsx
+++ frontend/src/components/Signup.tsx
@@ -99,15 +99,14 @@ function Signup()
     };
 
     return (
-        <div id="signupDiv" className="accountBox">
-            <h1 className="p-6 font-bold">Signup</h1>
+        <div id="signupDiv">
+            <h1>Signup</h1>
             <form id="signupInput" onSubmit={doSignup}> {/* Form wrapper */}
-                <div id="fullNameDiv" className="text-left py-1">
+                <div id="fullNameDiv">
                     <div id="fullNameLabel">
                         <label>Full Name</label>
                     </div>
                     <input
-                        className="accountInput"
                         type="text"
                         id="signupFullName"
                         placeholder="Full Name"
@@ -116,12 +115,11 @@ function Signup()
                         disabled={isLoading} // Disable on load
                     />
                 </div>
-                <div id="emailDiv" className="text-left py-1">
+                <div id="emailDiv">
                     <div id="emailLabel">
                         <label>Email</label>
                     </div>
                     <input
-                        className="accountInput"
                         type="email"
                         id="signupEmail"
                         placeholder="Email"
@@ -130,12 +128,11 @@ function Signup()
                         disabled={isLoading}
                     />
                 </div>
-                <div id="passwordDiv" className="text-left py-1">
+                <div id="passwordDiv">
                     <div id="passwordLabel">
                         <label>Password</label>
                     </div>
                     <input
-                        className="accountInput"
                         type="password"
                         id="signupPassword"
                         placeholder="Password"
@@ -144,12 +141,11 @@ function Signup()
                         disabled={isLoading}
                     />
                 </div>
-                <div id="passwordConfirmDiv" className="text-left py-1">
+                <div id="passwordConfirmDiv">
                     <div id="passwordConfirmLabel">
                         <label>Re-enter password</label>
                     </div>
                     <input
-                        className="accountInput"
                         type="password"
                         id="passwordConfirm"
                         placeholder="Confirm Password"
@@ -162,7 +158,6 @@ function Signup()
                     <span id="signupResult">{message}</span> {/* Feedback */}
                 </div>
                 <input
-                    className="bg-[#598392] text-white font-bold px-4 py-3 rounded-[10px] my-3 hover:bg-[#90b0bb]"
                     type="submit"
                     id="submitButton"
                     value={isLoading ? "Creating Account..." : "Create Account"} // Loading text
