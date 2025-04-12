e14290558858d5bad7e265c4fec31127cc0b2ffd
SarahD3904 <sarahd3904@gmail.com>
2025-04-12 10:54:26 -0400

    Changes to profile info displayed and fields for editing Minor changes to style

--- frontend/src/components/InfoBox.tsx
+++ frontend/src/components/InfoBox.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect, ReactElement, ReactHTMLElement } from 'react';
 function InfoBox()
 {
     var _ud = localStorage.getItem('user_data');
@@ -7,26 +8,101 @@ function InfoBox()
         return;
     }
     var userData = JSON.parse(_ud);
-    //var userId = userData.id;
-    var userName = userData.name;
+    var userId = userData.id;
+    //var userName = userData.name;
+
+    const app_name = "cop4331.tech";
+    function buildPath(route: string) : string {
+        if (process.env.NODE_ENV != "production") {
+            return 'http://localhost:5001' + route;
+        } else {
+            return 'http://' + app_name + route;
+        }
+    }
+
+    //profile info
+    const [fullName, setFullName] = useState('');
+    const [communication, setCommunication] = useState('');
+    const [skills, setSkills] = useState([]);
+    const [roles, setRoles] = useState([]);
+    const [interests, setInterests] = useState([]);
+
+    const getProfile = async () =>
+    {
+            
+        const obj = {id: userId};
+        const js = JSON.stringify(obj);
+
+        try{
+            const response = await fetch(buildPath('/api/get-user-info'),
+            {
+                method: 'POST',
+                body: js,
+                headers: { 'Content-Type': 'application/json' }
+            });
+            const res = await response.json(); // Parse response
+
+            if(res._id > 0)
+            {
+                alert('ERROR');
+            }
+            else
+            {
+                setFullName(res.name);
+                setCommunication(res.comm);
+                setSkills(res.skills);
+                setRoles(res.roles);
+                setInterests(res.interests);
+            }
+        }
+        catch(error: any){
+            alert('Error');
+
+        }
+    }
+
+    //fetch profile info upon loading
+    useEffect(() => {
+
+        getProfile();
+    }, []);
+
+    //converts skills array into a string
+    const stringArray = (theArray: string[]) =>
+    {
+        let newString = '';
+        if(theArray.length > 0)
+        {
+            for(let i=0; i<theArray.length - 1; ++i)
+            {
+                newString += theArray[i] + ", "
+            }
+            newString += theArray[theArray.length-1];
+        }
+        return newString;
+    }
     
     return(
         <div id="information">
             <div id="profName">
                 <p>Name:</p>
-                <p>{userName}</p>
+                <p>{fullName}</p>
             </div>
-            <div id="profDescription" className="mt-2">
-                <p>Description:</p>
-                <p>I am a coder.</p>
+            <div id="profCommunication" className="mt-2">
+                <p>Preferred Communication Method:</p>
+                <p>{communication}</p>
             </div>
             <div id="profSkills" className="mt-2">
                 <p>Skills:</p>
-                <p>I have coding skills.</p>
+                <p>{stringArray(skills)}</p>
+            </div>
+            <div id="profRoles" className="mt-2">
+                <p>Roles:</p>
+                <p>{stringArray(roles)}</p>
             </div>
             <div id="profInterests" className="mt-2">
                 <p>Interests:</p>
-                <p>I am interested in coding.</p>
+                <p>{stringArray(interests)}</p>
             </div>
         </div>
     );
