232a149dfc35a69e68208622d8fe71ca6138bbfb
SarahD3904 <sarahd3904@gmail.com>
2025-04-13 15:29:17 -0400

    Attempt to make dynamic list of search cards. Needs to be fixed.

--- frontend/src/components/ProjectSearch.tsx
+++ frontend/src/components/ProjectSearch.tsx
@@ -1,5 +1,7 @@
+import React from 'react';
+import { renderToString } from 'react-dom/server';
 
-function ProjectSearch()
+function ProjectSearch(this: any)
 {
     var _ud = localStorage.getItem('user_data');
     if(_ud == null) //redirect if user not found
@@ -8,15 +10,58 @@ function ProjectSearch()
         return;
     }
 
+    const createNewCard = (num: number) =>
+    {
+        var theKey = "item" + num;
+        return(
+            <li key={theKey}>
+                <div className="projSearchCard">
+                    <p>Project Name</p>
+                    <p className="text-right"># of Members</p>
+                    <div className="projSearchCardDesc">Description here</div>
+                    <button type="button" onClick={projMoreInfo}>Test</button>
+                </div> 
+            </li>
+        );
+    }
+
+    createNewCard.onClick = () =>
+    {
+        projMoreInfo;
+    }
+
+    function doSearch(event: React.MouseEvent)
+    {
+        event.preventDefault();
+        var theList = document.getElementById("resultList");
+        if(theList == null ) return;
+        var t = createNewCard(2);
+        theList.innerHTML += renderToString(t);
+    }
+
+    function projMoreInfo()
+    {
+        alert("WIP");
+    }
+
     return(
         <div id="searchBox" className="searchBox">
             <div id="searchArea">
                 <input type="text" id="searchBar" placeholder="Search Projects" />
-                <button type="button" id="searchButton" className="searchButton">Search</button>
+                <button type="button" id="searchButton" className="searchButton" onClick={doSearch}>Search</button>
             </div>
-            <div id="searchResults" className="searchResults">
-                <p>TEST</p>
-            </div>
+            <span id="searchResults" className="searchResults">
+                <ul id="resultList">
+                    <li>
+                        <div className="projSearchCard">
+                            <p>Project Name</p>
+                            <p className="text-right"># of Members</p>
+                            <div className="projSearchCardDesc">Description here</div>
+                            <button type="button" onClick={projMoreInfo}>Test</button>
+                        </div> 
+                    </li>
+                </ul>
+            </span>
         </div>
 
     );
