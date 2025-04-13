22cd595411a49b166234a9e8315125f5c7a7e9a0
SarahD3904 <sarahd3904@gmail.com>
2025-04-13 12:00:29 -0400

    Added beginning of search page. Currently has a search bar and button. Currently nonfunctional.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,7 @@ import ResetPasswordPage from './pages/ResetPasswordPage';
 import { getVersion, isProd } from './utils';
 import ProjectPage from './pages/ProjectPage';
 import UserProfilePage from './pages/UserProfilePage';
+import BrowseProjectsPage from './pages/BrowseProjectsPage';
 
 function App() {
 
@@ -28,6 +29,7 @@ function App() {
                 <Route path="/register" element={<SignupPage />} />
                 <Route path="/dashboard" element={<DashboardPage />} />
                 <Route path="/userprofile" element={<UserProfilePage />} />
+                <Route path="/browse" element={<BrowseProjectsPage />} />
                 <Route path="/forgot-password" element={<ForgotPasswordPage />} />
                 <Route path="/reset-password" element={<ResetPasswordPage />} />
                 <Route path="/projects/:project_id" element={<ProjectPage />} />
