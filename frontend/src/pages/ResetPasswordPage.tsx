a49d699d225c4525f0a919e19a09dce2469c2b3f
Artyboo-UCF <ad085158@ucf.edu>
2025-04-14 16:52:55 -0400

    Added Reset Password Page

---
+++ frontend/src/pages/ResetPasswordPage.tsx
@@ -0,0 +1,13 @@
+import ResetPassword from '../components/ResetPassword';
+import HeaderBar from '../components/HeaderBar';
+
+const ResetPasswordPage = () => {
+    return (
+        <div>
+            <HeaderBar />
+            <ResetPassword />
+        </div>
+    );
+};
+
+export default ResetPasswordPage;
