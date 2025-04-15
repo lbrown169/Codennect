c3593f760ccd961a5b99644735497987746c8e40
Artyboo-UCF <ad085158@ucf.edu>
2025-04-15 05:09:44 -0400

    Added formatting for other user profile page

---
+++ frontend/src/pages/OtherUserProfilePage.tsx
@@ -0,0 +1,13 @@
+import OtherUserProfile from '../components/OtherUserProfile';
+import HeaderBar from '../components/HeaderBar';
+
+const OtherUserProfilePage = () => {
+    return (
+        <div>
+            <HeaderBar />
+            <OtherUserProfile />
+        </div>
+    );
+};
+
+export default OtherUserProfilePage;
