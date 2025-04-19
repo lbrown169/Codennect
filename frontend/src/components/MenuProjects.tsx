f8f4c153ad901c50edb675635ad1ea29e037e3c8
SarahD3904 <sarahd3904@gmail.com>
2025-04-19 14:13:16 -0400

    Added project display card. Still needs link to details.

--- frontend/src/components/MenuProjects.tsx
+++ frontend/src/components/MenuProjects.tsx
@@ -77,7 +77,7 @@ function MenuProjects()
         domain: "",
         owner: "",
         is_private: false,
-        description: "A testing project for a testing world, with a testing long description for a testing-ly long purpose.",
+        description: "Another testing project for a testing world, with a testing long description for a testing-ly long purpose.",
         fields: [],
         roles: { "Frontend": 2, "Backend": 2 },
         users: { "Frontend": [], "Backend": []},
@@ -106,12 +106,39 @@ function MenuProjects()
         }
     }
 
-    //mapping reference
-    //{projects.map((project) => ( ))}
+    function getCapacity(project: Project)
+    {
+        let currentCap = 0;
+        let maxCap = 0;
+        for(const role in project.users)
+            currentCap += project.users[role].length;
+        for(const role in project.roles)
+            maxCap += project.roles[role];
+
+        return '(' + currentCap + '/' + maxCap + ')';
+    }
+
+    function getAvailableRoles(project: Project)
+    {
+        const theRoles: string[] = [];
+        for(const role in project.roles)
+        {
+            theRoles.push(role);
+        }
+        if(!theRoles)   return 'No roles';
+        let newStr = theRoles.join(', ');
+        return newStr;
+    }
+
+    function requiredSkillList(project: Project)
+    {
+        if(!project.required_skills)    return 'No skills required';
+        return project.required_skills.join(", ");
+    }
 
     return(
-        <div id="mainMenuBox" className="bg-[#EFF6E0] p-4 w-9/10 m-auto">
-            <div id="sliderBox" className="text-white text-lg mb-4">
+        <div id="mainMenuBox" className="bg-[#EFF6E0] p-4 w-9/10 m-auto rounded-xl">
+            <div id="sliderBox" className="text-white text-lg mb-4 border-black border-1 rounded-md">
                 <SegmentedControl
                     
                     color='#124559'
@@ -126,8 +153,24 @@ function MenuProjects()
             </div>
             <div id="resultsBox">
                 {resultDisplay.map((project) => (
-                    <div id={project._id} className="text-black">
-                        <p>Name: {project.name}</p>
+                    <div key={project._id} className="border-white border-4 rounded-xl text-black drop-shadow-md">
+                        <div className="flex bg-white p-2">
+                            <h3 className="font-bold text-lg">Name: {project.name} {getCapacity(project)}</h3>
+                        </div>
+                        <div className="m-2">
+                            <p className="text-gray-500 uppercase">Description</p>
+                            <p className="font-medium">{project.description}</p>
+                        </div>
+                        <div className="m-2 flex grow gap-10 items-start">
+                            <div id="openRoles" className="flex flex-col flex-[1_0_0] justify-center">
+                                <p className="text-gray-500 uppercase">Roles</p>
+                                <p className="text-wrap font-medium">{getAvailableRoles(project)}</p>
+                            </div>
+                            <div id="reqSkills" className="flex flex-col flex-[1_0_0] justify-center">
+                                <p className="text-gray-500 uppercase">Skills</p>
+                                <p className="text-wrap font-medium">{requiredSkillList(project)}</p>
+                            </div>
+                        </div>
                     </div>
                 ))}
             </div>
