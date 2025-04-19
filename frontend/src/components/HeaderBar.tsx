a83dc4a344f07556d05746684c35d247acb0437a
Artyboo-UCF <ad085158@ucf.edu>
2025-04-19 02:48:27 -0400

    Added Logo

--- frontend/src/components/HeaderBar.tsx
+++ frontend/src/components/HeaderBar.tsx
@@ -1,10 +1,7 @@
-function HeaderBar()
-{
-    const goHome = (_event: React.MouseEvent) =>
-    {
+function HeaderBar() {
+    const goHome = (_event: React.MouseEvent) => {
         var _ud = localStorage.getItem('user_data');
-        if(_ud == null) //redirect if user not found
-        {
+        if (_ud == null) {
             window.location.href = '/';
             return;
         }
@@ -12,32 +9,36 @@ function HeaderBar()
         return;
     };
 
-    const doLogout = (event:any) =>
-    {
+    const doLogout = (event: any) => {
         event.preventDefault();
         localStorage.removeItem("user_data");
         window.location.href = '/';
     };
 
     var _ud = localStorage.getItem('user_data');
-    if(_ud == null) //default header bar
-    {
-        return(
-            <div id="headerBar" className="navBar">
+    if (_ud == null) {
+        return (
+            <div id="headerBar" className="navBar flex items-center space-x-4">
+                <a href="#" onClick={goHome}>
+                    <img src="/logo.png" alt="Logo" className="h-8" />
+                </a>
                 <a href="#" onClick={goHome}>Home</a>
                 <a href="/register">Register</a>
             </div>
         );
     }
 
-    //logged in header
-    return(
-        <div id="headerBar" className="navBar space-x-4">
+    return (
+        <div id="headerBar" className="navBar flex items-center space-x-4">
+            <a href="#" onClick={goHome}>
+                <img src="/logo.png" alt="Logo" className="h-8" />
+            </a>
             <a href="#" onClick={goHome}>Home</a>
             <a href="/userprofile">Profile</a>
             <a href="/browse">Browse Projects</a>
             <a href="#" className="float-right inset-y-[-8px]" onClick={doLogout}>Log Out</a>
         </div>
     );
-};
+}
+
 export default HeaderBar;
