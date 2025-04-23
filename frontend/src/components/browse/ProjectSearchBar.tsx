4d0d2a2de8c5c4c920c3f05bcf54e503a743eceb
SarahD3904 <sarahd3904@gmail.com>
2025-04-23 16:58:36 -0400

    Search functions. May need more testing.

--- frontend/src/components/browse/ProjectSearchBar.tsx
+++ frontend/src/components/browse/ProjectSearchBar.tsx
@@ -4,6 +4,7 @@ import { ProjectCard } from "../dashboard/ProjectsPanel";
 import { UserContext } from '../../hooks/UserContext';
 import { FaSearch } from "react-icons/fa";
 import { Alert, Button, Combobox, useCombobox, CloseButton, Grid, Input, InputBase, MultiSelect, ScrollArea, Skeleton, TextInput } from "@mantine/core";
+import { getSearchResults } from "../../api/ProjectAPI";
 
 import { IoMdInformationCircleOutline } from 'react-icons/io';
 
@@ -63,11 +64,13 @@ export function ProjectSearchBar()
         return <Skeleton />;
     }
 
+    /*
     const projects = user.projects.filter((project) =>
         Object.values(project.users)
             .map((role) => role.users.includes(user._id))
             .includes(true)
     );
+    */
 
     const comboOptions = roleOptions.map((role) => (
         <Combobox.Option value={role} key={role}>
@@ -78,34 +81,51 @@ export function ProjectSearchBar()
     async function doSearch(event: React.FormEvent)
     {
         event.preventDefault();
-        alert("WIP");
         try {
-            let query = '';
+            let n = '';
+            let s = '';
+            let r = '';
             if(searchInput || skillValue.length > 0 || roleValue)
             {
-                query += '?';
                 if(searchInput)
                 {
-                    query += new URLSearchParams({ name: searchInput }).toString();
+                    //n += new URLSearchParams({ name: searchInput }).toString();
+                    n += searchInput.toString();
                 }
                 if(skillValue.length > 0)
                 {
-                    if(query.length > 1)   query += '&';
-                    query += new URLSearchParams({ required_skills: skillValue.toString() }).toString();
+                    //if(query.length > 1)   query += '&';
+                    //query += new URLSearchParams({ required_skills: skillValue.toString() }).toString();
+                    s += skillValue.toString();
                 }
                 if(roleValue)
                 {
-                    if(query.length > 1)    query += '&';
-                    query += new URLSearchParams({ roles: roleValue });
+                    //if(query.length > 1)    query += '&';
+                    //query += new URLSearchParams({ roles: roleValue });
+                    r += roleValue.toString();
                 }
                 //query += new URLSearchParams({ name: searchInput, required_skills: skillValue.toString(), roles: roleString }).toString();
-                alert(query); 
                 
             }
 
+            const query = {
+                name: n,
+                required_skills: s,
+                roles: r,
+            }
 
-            const newProjects = projects.filter(    //filter out owned projects
-                (project) => project.owner !== user?._id
+            const response = await getSearchResults(query);
+            const res = await response.json();
+
+            if(res.error)
+            {
+                alert(res.error);
+                return;
+            }
+            const allResults = res.result;
+
+            const newProjects = allResults.filter(    //filter out owned projects
+                (project: Project) => project.owner !== user?._id
             );
             setSearchResults(newProjects);
         }
