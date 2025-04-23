26660fb07f51e53f6a09341ac17ea2a7f0426148
SarahD3904 <sarahd3904@gmail.com>
2025-04-23 16:58:36 -0400

    Search functions. May need more testing.

--- frontend/src/api/ProjectAPI.ts
+++ frontend/src/api/ProjectAPI.ts
@@ -4,4 +4,14 @@ export async function getProject(project_id: string) {
     return await getRequest(`/api/projects/${project_id}`);
 }
 
+export async function getSearchResults(q: Record<string,string>) {
+    let header = {
+        'Content-Type': 'application/json',
+    }
+    let option = {
+        headers: header,
+        query: q,
+    }
+    return await getRequest('/api/projects', option);
+}
 
